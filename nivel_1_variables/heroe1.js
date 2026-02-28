ad5e9a204d8fcdbf79c57dc4cffb5d652567bd72
Estudiantes <estudiantes@sprint0.com>
2026-02-28 08:57:02 -0500

    first commit

---
+++ nivel_1_variables/heroe1.js
@@ -0,0 +1,11 @@
+// heroe1.js 
+let nombreHeroe = "Batman";
+let poder = "Inteligencia y artes marciales";
+let nivelEnergia = 80;
+let ciudad = "Gotham";
+let enemigo = "Joker";
+
+console.log("--- Reporte de Misión ---");
+console.log(nombreHeroe + " está defendiendo " + ciudad + " de " + enemigo + ".");
+console.log("Su nivel de energía actual es de " + nivelEnergia + "%.");
+console.log("Usará su " + poder + " para ganar.");
---
+++ nivel_1_variables/heroe2.js
@@ -0,0 +1,11 @@
+// heroe2.js
+let nombreHeroe = "Spider-Man";
+let poder = "Lanzar telarañas y sentido arácnido";
+let nivelEnergia = 90;
+let ciudad = "Nueva York";
+let enemigo = "Duende Verde";
+
+console.log("--- Reporte de Patrullaje ---");
+console.log(nombreHeroe + " se balancea por los edificios de " + ciudad + ".");
+console.log("De repente aparece el " + enemigo + ".");
+console.log("¡Con " + nivelEnergia + " de energía y su " + poder + ", está listo para luchar!");
