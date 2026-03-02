d4b75a84c290009dc55793fed0fa00e71033f0d4
Esteban2267 <es8429838@gmail.com>
2026-03-02 08:54:35 -0500

    Actualizar ciudad de Batman en heroe1.js

--- nivel_1_variables/heroe1.js
+++ nivel_1_variables/heroe1.js
@@ -2,7 +2,7 @@
 let nombreHeroe = "Batman";
 let poder = "Inteligencia y artes marciales";
 let nivelEnergia = 80;
-let ciudad = "Gotham";
+let ciudad = "Los angeles";
 let enemigo = "Joker";
 
 console.log("--- Reporte de Misión ---");
